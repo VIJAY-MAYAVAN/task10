db52332ab94ade51553ee1f1b0164bfa8191976e
VIJAY-MAYAVAN <yajivv25@gmail.com>
2023-11-23 21:58:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,59 @@
+let timer=10;
+let time = document.getElementById('display');
+time.style.width= "50%";
+setTimeout(() => {
+    time.innerHTML = timer--;
+    time.style.color = "black"
+setTimeout(() =>{
+    time.className = "text";
+    time.innerHTML = timer--;
+    time.style.color = "red";
+setTimeout(() =>{
+    time.className = "text";
+    time.innerHTML = timer--;
+    time.style.color = "black"
+setTimeout(() =>{
+    time.className = "text";
+    time.innerHTML = timer--;
+    time.style.color = "red";
+setTimeout(() =>{
+    time.className = "text";
+    time.innerHTML = timer--;
+    time.style.color = "black"
+setTimeout(() =>{
+    time.className = "text";
+    time.innerHTML = timer--;
+    time.style.color = "red";
+setTimeout(() =>{
+    time.className = "text";
+    time.innerHTML = timer--;
+    time.style.color = "black"
+setTimeout(() =>{
+    time.className = "text";
+    time.innerHTML = timer--;
+    time.style.color = "red";
+setTimeout(() =>{
+    time.className = "text";
+    time.innerHTML = timer--;
+    time.style.color = "black"
+setTimeout(() =>{
+    time.className = "text";
+    time.innerHTML = timer--;
+    time.style.color = "red";
+setTimeout(() =>{
+    time.style.fontSize = "50px";
+    time.style.maxWidth = "500px";
+    time.style.color = "blue";
+    time.className = "text";
+    time.innerHTML = "Happy Independance Day";                                          
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)
+    },1000)     
+    },1000)
+    },1000)
+    },1000); 
